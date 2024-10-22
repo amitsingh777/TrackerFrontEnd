5a422df0a648f97fd5fcbc7c9104d4919a97a4d2
Amit Singh <amitsingh@Amits-Laptop.local>
2024-10-22 13:25:40 +0530

    Created custom text input

--- src/components/CustomTextInput.tsx
+++ src/components/CustomTextInput.tsx
@@ -1,4 +1,4 @@
-import {StyleSheet, TextInput} from 'react-native';
+import {StyleSheet, TextInput, View} from 'react-native';
 import {TextInputProps} from 'react-native/Libraries/Components/TextInput/TextInput';
 //circle-with-cross
 import Icon from 'react-native-vector-icons/Entypo';
@@ -7,24 +7,42 @@ import FontIcon from 'react-native-vector-icons/FontAwesome';
 const CustomTextInput = (props: TextInputProps) => {
   return (
     <>
-      <TextInput
-        {...props}
-        style={inputStyle.input}
-        placeholderTextColor="white"
-      />
-      <Icon name="circle-with-cross" size={30} color="black" />
-      <FontIcon name="rocket" size={30} color="#900" />
+      <View style={inputStyle.container}>
+        <TextInput
+          {...props}
+          style={inputStyle.input}
+          placeholderTextColor="#ffffff80"
+          inlineImageLeft="d"
+        />
+        <Icon
+          name="circle-with-cross"
+          size={15}
+          color="black"
+          style={inputStyle.icon}
+        />
+      </View>
     </>
   );
 };
 
 const inputStyle = StyleSheet.create({
   input: {
-    backgroundColor: 'black',
+    backgroundColor: '#00000070',
     opacity: 0.4,
     borderRadius: 6,
-    paddingHorizontal: 6,
+    // paddingHorizontal: 6,
     paddingVertical: 3,
+    paddingRight: 26,
+    paddingLeft: 6,
+    color: 'white',
+  },
+  container: {
+    flexGrow: 0,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    marginLeft: -23,
   },
 });
 
